e2aa5768dac5946899633d72fb9beda5693009eb
Abraham <abraao.matos236@gmail.com>
2025-03-02 11:24:26 -0400

    feat: add Search component and update App.jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-const app = () => {
+import { useState } from "react";
+import Search from "./components/Search";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+      </div>
+    </main>
   );
 };
 
-export default app;
+export default App;
