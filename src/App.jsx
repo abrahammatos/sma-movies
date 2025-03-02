4e2848ef78cfccf246937e15953897f6a9d6e7e2
Abraham <abraao.matos236@gmail.com>
2025-03-02 13:40:33 -0400

    feat: connect to movie api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./components/Search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok)
+        throw new Error("Something went wrong with fetching movies");
+
+      const data = await response.json();
+
+      if (data.response === "false") {
+        setErrorMessage(data.Error || "Something went wrong.");
+        setMovies([]);
+        return;
+      }
+
+      setMovies(data.results || []);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage("An error occurred while fetching movies.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <main>
       <div className="pattern" />
@@ -18,6 +65,12 @@ const App = () => {
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   );
